f96398a986f36b81d1aa2f4ad2891df851acda56
SubjectCarter <32699657@student.murdoch.edu.au>
2021-06-13 10:33:17 -0400

    Youtube Projects Folder

---
+++ Youtube Projects/Madlibs.py
@@ -0,0 +1,16 @@
+#Starting with a comment
+Youtube = "Pewdiepie"
+
+adj = input("Adjective: ")
+verb1 = input("Verb :")
+verb2 = input("Verb :")
+famous_person = input("Famous Person: ")
+
+madlib = f"Computer Programming is so {adj}! It makes me so excited all the time\
+    I love to {verb1}. stay hydrated and {verb2} like you are {famous_person} "
+
+print(madlib)
+
+
+
+exit()
